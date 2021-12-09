0f1fc04d7687d2311f3c27dc9e47bcd4c4acc82a
梁俊可 <2583493258@qq.cpm>
2021-12-09 17:46:03 +0800

    Initial commit

---
+++ Day01_JDBC/src/com/nboweb/jdbc/JDBCDemo.java
@@ -0,0 +1,32 @@
+package com.nboweb.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * JDBC快速入门
+ */
+public class JDBCDemo {
+    public static void main(String[] args) throws Exception{
+        //01、注册驱动
+        //Class.forName("com.mysql.jdbc.Driver");
+        //02、获取连接u
+        //String url="jdbc:mysql://127.0.0.1:3306/db1";
+        String url="jdbc:mysql:///db1?useSSL=false";
+        String username="root";
+        String password="liang521";
+        Connection conn = DriverManager.getConnection(url, username, password);
+        //03、定义SQL语句
+        String sql="UPDATE tb_user SET password=1234 WHERE id=2;";
+        //04、获取执行sql的对象Statement
+        Statement stmt=conn.createStatement();
+        //05、执行sql
+        int count = stmt.executeUpdate(sql);//返回受影响的行数
+        //06、处理结果
+        System.out.println(count);
+        //07、释放资源
+        stmt.close();
+        conn.close();
+    }
+}
