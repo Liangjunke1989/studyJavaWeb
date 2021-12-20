8114c24ca138cc69fd8b9c09e37e8c3972d2acfd
梁俊可 <2583493258@qq.cpm>
2021-12-20 10:19:42 +0800

    ljk20211220_MyBatis_单条件动态查询

--- Day02_MyBatis/src/main/java/com/nboweb/mapper/BrandMapper.java
+++ Day02_MyBatis/src/main/java/com/nboweb/mapper/BrandMapper.java
@@ -16,21 +16,30 @@ public interface BrandMapper {
      */
     Brand selectById(int id);
 
-    /**
-     * 查询：条件查询
-     *  *参数接收：
-     *      1、散装参数
-     *          如果方法中有多个参数，需要使用@Param("SQL参数占位符名称")类型
-     *      2、对象参数
-     *          对象属性的名称要和参数占位符名称一致
-     *      3、Map集合参数
-     * @param status 状态
-     * @param companyName 公司名称
-     * @param brandName 商品名称
-     * @return 商品对象
-     */
+//    /**
+//     * 查询：条件查询
+//     *  *参数接收：
+//     *      1、散装参数
+//     *          如果方法中有多个参数，需要使用@Param("SQL参数占位符名称")类型
+//     *      2、对象参数
+//     *          对象属性的名称要和参数占位符名称一致
+//     *      3、Map集合参数
+//     * @param status 状态
+//     * @param companyName 公司名称
+//     * @param brandName 商品名称
+//     * @return 商品对象
+//     */
        // List<Brand> selectByCondition(@Param("status") int status,@Param("companyName") String companyName,@Param("brandName") String brandName);
       // List<Brand> selectByCondition(Brand brand);
-       List<Brand> selectByCondition(Map map);
+
+    /**
+     *
+     * @param map map类型
+     * @return brand商品类
+     */
+    List<Brand> selectByCondition(Map map);
+
+
+    List<Brand> selectByConditionSingle(Brand brand);
 
 }
